b5916c920872130225c01f3bdfcc18b7ae9387c4
ikerismak <ikerismak@gmail.com>
2023-02-16 21:32:15 -0600

    last

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -7,9 +7,9 @@ export const Principal = () => {
   return (
     <div className='home'>
       <h1> Hi im <strong>Iker Toscano</strong> I am a mechatronics engineer with 3 years of experience in
-manufacturing as a product development and customer delivery
+manufacturing as a product developer and prgram managger
 planner. I am a master's student in software engineering and an
-aspirant <strong>web developer</strong> .</h1>
+aspirant to <strong>web developer</strong> .</h1>
 
       <h2><Link to="/contacto" className='contact-home'>Contact</Link></h2>
       <section>
